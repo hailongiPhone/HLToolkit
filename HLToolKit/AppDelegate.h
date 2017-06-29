62c19e0281f9c1d004e745265537293af8dc254f
hailong <ghl_hh@hotmail.com>
2017-06-29 13:59:17 +0900

    Initial Commit

---
+++ HLToolKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLToolKit
+//
+//  Created by hl on 29/06/2017.
+//  Copyright © 2017 HL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLToolKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HLToolKit
+//
+//  Created by hl on 29/06/2017.
+//  Copyright © 2017 HL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
